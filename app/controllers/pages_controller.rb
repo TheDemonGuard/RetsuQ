a2b3fd207d962c929fbecd19913da14636206dab
Bonny Boet <bonny.boet@gmail.com>
2022-08-09 22:18:24 +0900

    Pages controller - Add variable for restaurants and markers for map

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,5 +2,13 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home ]
 
   def home
+    @restaurants = Restaurant.all
+
+    @markers = @restaurants.geocoded.map do |flat|
+      {
+        lat: flat.latitude,
+        lng: flat.longitude
+      }
+    end
   end
 end
