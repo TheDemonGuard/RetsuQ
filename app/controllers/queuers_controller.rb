722689ca5c25d2365b89370a3b1d003167b27fe0
Ethan Tadokoro <etado1408@gmail.com>
2022-08-09 21:55:27 +0900

    added owner ability to view queue

--- app/controllers/queuers_controller.rb
+++ app/controllers/queuers_controller.rb
@@ -1,2 +1,31 @@
 class QueuersController < ApplicationController
+  def index
+    @queuers = Queuer.find(params[:restaurant_id])
+  end
+
+  def show
+    @queuers = Queuer.where(restaurant_id: params[:id])
+  end
+
+  def new
+    @queue = Queuer.new
+  end
+
+  def create
+    @user_id = current_user.id
+    @queue = Queuer.new(queue_params)
+    @queue.user_id = @user_id
+    if @queue.save
+      redirect_to queuer_path(@queue)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def queue_params
+    params.require(:queue).permit(:size, :user_id, :restaurant_id)
+  end
+
 end
