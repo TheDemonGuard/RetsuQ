f33238ee7af065905f236f6b8bf9a30f2adf50c2
Payla Vargas <miapaylagvargas@gmail.com>
2022-08-19 00:44:11 +0900

    fix bug in dashboard action to show the first active queuer, fix the bug in queuers#show to show only the number of of active queuers in the view, change placeholder in size in new queuer pafe, add active queuers seeds for The Gyoza Academy

--- app/controllers/owner_controller.rb
+++ app/controllers/owner_controller.rb
@@ -2,16 +2,14 @@ class OwnerController < ApplicationController
   def dashboard
     @restaurant = current_user.restaurant
     @queuers = Queuer.where(restaurant_id: @restaurant, status: "queueing")
+    @next_queuer = Queuer.where(restaurant_id: current_user.restaurant, status: "queuing").first
+    if @next_queuer.nil?
+      redirect_to owner_path, notice: "No queuers in queue"
+    end
   end
 
   def queuers
     @queurs = Queuer.all
   end
-
-  def next_queuer
-    @queuer = Queuer.where(restaurant_id: current_user.restaurant, status: "queueing").first
-    if @queuer.nil?
-      redirect_to owner_path, notice: "No queuers in queue"
-    end
-  end
+  
 end
