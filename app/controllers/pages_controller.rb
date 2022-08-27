b080e401f222dacfc1eb2060dfaef58ca5ebe829
Bonny Boet <bonny.boet@gmail.com>
2022-08-27 21:15:09 +0900

    Add filter by distance feature to home page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,14 +3,23 @@ class PagesController < ApplicationController
 
   def home
     @restaurants = Restaurant.all
+    if params[:lat]
+      # params[:dist] = 1000 (for demo from Osaka)
+      @near_restaurants = Restaurant.near([params[:lat], params[:long]], params[:dist])
+    else
+      @markers = @restaurants.geocoded.map do |restaurant|
+        {
+          lat: restaurant.latitude,
+          lng: restaurant.longitude,
+          image_url: helpers.asset_url("Location_Pin_Centered.png"),
+          info_window: render_to_string(partial: "info_window", locals: { restaurant: restaurant })
+        }
+      end
+    end
 
-    @markers = @restaurants.geocoded.map do |restaurant|
-      {
-        lat: restaurant.latitude,
-        lng: restaurant.longitude,
-        image_url: helpers.asset_url("Location_Pin_Centered.png"),
-        info_window: render_to_string(partial: "info_window", locals: { restaurant: restaurant })
-      }
+    respond_to do |format|
+      format.html
+      format.json
     end
   end
 end
---
+++ app/javascript/controllers/nearrestaurants_controller.js
@@ -0,0 +1,31 @@
+import { Controller } from "@hotwired/stimulus"
+
+export default class extends Controller {
+  static targets = ["nearyou"]
+
+  connect() {
+    this.crd = {}
+    navigator.geolocation.getCurrentPosition(this.success.bind(this))
+  }
+
+  success(pos) {
+    this.crd = pos.coords;
+
+    console.log('Your current position is:');
+    console.log(`Latitude : ${this.crd.latitude}`);
+    console.log(`Longitude: ${this.crd.longitude}`);
+    console.log(`More or less ${this.crd.accuracy} meters.`);
+  };
+
+  getRestaurants(event) {
+    const maxDistance = event.currentTarget.value
+    console.log(this.crd, maxDistance)
+    fetch(window.location.origin + "?lat=" + this.crd.latitude + "&long=" + this.crd.longitude + "&dist=" + maxDistance, {
+      headers: { "Accept": "application/json" },
+    })
+      .then(response => response.json())
+      .then((data) => {
+        this.nearyouTarget.innerHTML = data.restaurants
+      })
+  }
+}
