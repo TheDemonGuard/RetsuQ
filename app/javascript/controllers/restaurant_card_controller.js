da7dce9efb0fc5cd114d16511ea6df97e3053f9b
Payla Vargas <miapaylagvargas@gmail.com>
2022-08-30 21:28:26 +0900

    rerender the modal form on restaurant show page

--- app/javascript/controllers/restaurant_card_controller.js
+++ app/javascript/controllers/restaurant_card_controller.js
@@ -1,7 +1,7 @@
 import { Controller } from "stimulus"
 
 export default class extends Controller {
-  static targets = [ "join", "overlay", "status", "wait", "customers" ]
+  static targets = [ "join", "overlay", "status", "wait", "customers", "closed" ]
 
   connect() {
     console.log('Hello, Restaurant card!')
@@ -9,10 +9,10 @@ export default class extends Controller {
     if (this.statusTarget.innerText == "Open") {
       console.log(this.overlayTarget)
       this.overlayTarget.style.display = "none"
+      this.closedTarget.style.display = "none"
     }
     else {
-      this.joinTarget.classList.add("closed")
-      this.joinTarget.innerText = "Queue closed"
+      this.joinTarget.style.display = "none"
       this.waitTarget.classList.add("closed")
       this.customersTarget.classList.add("closed")
     }
